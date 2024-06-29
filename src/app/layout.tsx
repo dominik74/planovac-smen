b44828e893d4cf787b94632f597cbeae1e840119
dominik74 <60546513+dominik74@users.noreply.github.com>
2024-06-29 17:24:02 +0200

    Disable pinch to zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -8,6 +8,14 @@ export const metadata: Metadata = {
   title: "Plánovač směn",
   description: "Automatické předvídání nadefinovaného vzoru směnových dnů v kalendáři.",
 };
+
+export const viewport: Viewport = {
+    initialScale: 1,
+    maximumScale: 1,
+    minimumScale: 1,
+    width: "device-width",
+    userScalable: false
+}
 
 export default function RootLayout({
   children,
